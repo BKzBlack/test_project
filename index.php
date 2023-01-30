286385b2a86f33f9dc43a0790dcd5b5f7ce3aced
BKzBlack <41716886+BKzBlack@users.noreply.github.com>
2023-01-31 00:19:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+echo "ssssssssssssssss";
+die;
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
